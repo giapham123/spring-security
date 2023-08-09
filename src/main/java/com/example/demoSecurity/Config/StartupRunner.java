da4b63576a75a2307107f62937f40e0f9c17997c
giapham <giapham121994@gmail.com>
2023-08-09 13:58:12 +0700

    commit front end

--- src/main/java/com/example/demoSecurity/Config/StartupRunner.java
+++ src/main/java/com/example/demoSecurity/Config/StartupRunner.java
@@ -1,7 +1,5 @@
 package com.example.demoSecurity.Config;
 
-import com.mysql.jdbc.log.Log;
-import org.apache.juli.logging.LogFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
